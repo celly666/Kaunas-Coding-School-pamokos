c1568948e46f0674c28c42470bc8c26b01c70ffa
Ramunė Alionytė <66987985+celly666@users.noreply.github.com>
2020-07-27 19:59:10 +0300

    2020/06/22

---
+++ Atsiskaitymas iš klasių/Miestai_klases.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace adjajfdsfs
+{
+    class Miestai
+    {
+
+        public Miestai(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+                StreamReader stream = new StreamReader("miestai.txt");
+
+                string line;
+
+                List<Miestai> miestai = new List<Miestai>();
+                while ((line = stream.ReadLine()) != null)
+                {
+
+                    string[] eilute = line.Split(' ');
+
+
+                    var obj = new Miestai(eilute[0]);
+
+
+                    obj.Country = eilute[1];
+
+
+                    miestai.Add(obj);
+
+
+                }
+                while (miestai.Count >= 1)
+                {
+
+                    string valstybees = miestai[0].Country;
+
+                    int kiek = miestai.Where(p => p.Country == valstybees).Count();
+
+                    miestai.RemoveAll(p => p.Country == valstybees);
+
+
+                    Console.WriteLine("Valstybe {0} turi {1} miestu .", valstybees, kiek);
+                }
+
+                stream.Close();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Atsiskaitymas iš klasių/kambariai.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kambariu_klase_rezervacija
+{
+    class Program
+    {
+		class Kambarys
+		{
+			public int kambarioKlase;
+			public bool uzimta;
+			public int uzimtasLaikasDienom;
+			public int vietuSkaicius;
+			public double kainaZmogui;
+			public string kambarioAprasymas;
+			public bool nesutvarkytas;
+			public int apsigyvenusiuZmoniuSkaicius;
+
+			public Kambarys ()
+			{
+
+			}
+			public void checkIn ()
+			{
+
+			}
+
+			public void Sutvarkyti ()
+			{
+
+			}
+
+			public void Apmoketi ()
+			{
+
+			}
+
+			public void Informacija ()
+			{
+
+			}
+
+		}
+		static void Main(string[] args)
+        {
+
+        }
+    }
+}
